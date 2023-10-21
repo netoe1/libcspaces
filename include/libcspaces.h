86aab3fd575e25cb34e02d7f1b9b4460887902ac
netoe1 <elynetobr@outlook.com>
2023-10-21 20:22:32 -0300

    starting

---
+++ include/libcspaces.h
@@ -0,0 +1,15 @@
+#ifndef LIBCSPACES_H
+#define LIBCSPACES_H
+
+#define INT "%d"
+#define INT "%i"
+#define CHAR "%c"
+#define FLOAT "%f"
+#define DOUBLE "%lf"
+#define LONG "%ld"
+#define fprintf filePrint
+#define printf consoleOut
+#define scanf consoleIn
+#define sprintf stringSet
+
+#endif
